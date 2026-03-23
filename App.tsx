661403e870498c73013e79e61233e223dc36353f
Marco Antonio Peña Vargas <marc300716@gmail.com>
2026-03-22 18:02:54 -0600

    Base Ventana 1

--- App.tsx
+++ App.tsx
@@ -6,13 +6,31 @@ const Inicio = () =>{
 
   return(
     <View style={estilos.contPrin}>
-      <View style={estilos.contA}>
+      <View style={estilos.contVoc}>
         <Image
           source={require('./frontend/image/A.png')}
-          style={estilos.imgA}
+          style={[estilos.vocBase,estilos.posA]}
         />
-
-
+        <Image
+          source={require('./frontend/image/E.png')}
+          style={[estilos.vocBase, estilos.posicionE]}
+        />
+        <Image
+          source={require('./frontend/image/I1.png')}
+          style={[estilos.vocBase, estilos.posicionI]}
+        />
+        <Image
+          source={require('./frontend/image/Play.png')}
+          style={[estilos.contPlay, estilos.Play]}
+        />
+        <Image
+          source={require('./frontend/image/O.png')}
+          style={[estilos.vocBase, estilos.posicionO]}
+        />
+        <Image
+          source={require('./frontend/image/U.png')}
+          style={[estilos.vocBase, estilos.posicionU]}
+        />
       </View>
     </View>
   );
@@ -22,18 +40,53 @@ export default Inicio;
 const estilos = StyleSheet.create({
   contPrin:{
     flex: 1,
-    backgroundColor: '#87D1EE',
+    backgroundColor: '#386070',
   },
-    contA:{
+    contVoc:{
     flex: 1,
-    backgroundColor: '#87D1EE',
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: '#a8daee',
+    position:'relative'
   },
-  imgA:{
-    width: scale(60),
-    height: verticalScale(60),
+  vocBase: {
+    width: scale(55),
+    height: verticalScale(55),
     resizeMode: 'contain',
-    transform: [{rotate: '-50deg'}]
-  }
+    position: 'absolute'
+  },
+  posA: {
+    top:'60%',
+    left: '15%',
+    transform: [{rotate: '-60deg'}]
+  },
+  posicionE: {
+    top: '25%', 
+    left: '27%',
+    transform: [{ rotate: '-35deg' }]
+  },
+  posicionI: {
+    top: '15%', 
+    left: '45%', 
+    transform: [{ rotate: '0deg' }]
+  },
+  posicionO: {
+    top: '25%', 
+    right: '27%', 
+    transform: [{ rotate: '35deg' }]
+  },
+  posicionU: {
+    top: '62%', 
+    right: '17%',
+    transform: [{ rotate: '60deg' }]
+  },
+  contPlay: {
+    width: scale(80),
+    height: verticalScale(80),
+    resizeMode: 'contain',
+    position: 'absolute'
+  },
+  Play: {
+    top: '60%', 
+    left: '44%', 
+    transform: [{ rotate: '0deg' }]
+  },
 })
