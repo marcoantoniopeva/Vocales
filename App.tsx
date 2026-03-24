00409eb0c81af9158490e06c010af31a5f51329d
Marco Antonio Peña Vargas <marc300716@gmail.com>
2026-03-23 19:32:14 -0600

    Animado A y Audios

--- App.tsx
+++ App.tsx
@@ -1,32 +1,75 @@
-import React, { } from "react";
-import {View, StyleSheet, Image} from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, StyleSheet, Image, TouchableOpacity, Animated } from "react-native";
 import { scale, verticalScale } from "react-native-size-matters";
 
-const Inicio = () =>{
+const Inicio = ({ navigation }) => {
+  const animacionA = useRef(new Animated.Value(0)).current;
 
-  return(
+  useEffect(() => {
+    Animated.timing(animacionA, {
+      toValue: 1,
+      duration: 1000,
+      useNativeDriver: true,
+    }).start();
+  }, []);
+
+  const translateY_A = animacionA.interpolate({
+    inputRange: [0, 1],
+    outputRange: [200, 0],
+  });
+
+  const rotate_A = animacionA.interpolate({
+    inputRange: [0, 1],
+    outputRange: ['0deg', '-60deg'],
+  });
+
+  const opacity_A = animacionA.interpolate({
+    inputRange: [0, 1],
+    outputRange: [0, 1],
+  });
+
+  return (
     <View style={estilos.contPrin}>
       <View style={estilos.contVoc}>
-        <Image
+        <Animated.Image
           source={require('./frontend/image/A.png')}
-          style={[estilos.vocBase,estilos.posA]}
+          style={[
+            estilos.vocBase,
+            estilos.posA,
+            {
+              opacity: opacity_A,
+              transform: [
+                { translateY: translateY_A },
+                { rotate: rotate_A },
+              ]
+            }
+          ]}
         />
+
         <Image
           source={require('./frontend/image/E.png')}
           style={[estilos.vocBase, estilos.posicionE]}
         />
+        
         <Image
           source={require('./frontend/image/I1.png')}
           style={[estilos.vocBase, estilos.posicionI]}
         />
-        <Image
-          source={require('./frontend/image/Play.png')}
+
+        <TouchableOpacity
           style={[estilos.contPlay, estilos.Play]}
-        />
+          onPress={() => navigation.navigate('Menu')}> 
+          <Image
+            source={require('./frontend/image/Play.png')}
+            style={{ width: '100%', height: '100%', resizeMode: 'contain' }}
+          />
+        </TouchableOpacity>
+
         <Image
           source={require('./frontend/image/O.png')}
           style={[estilos.vocBase, estilos.posicionO]}
         />
+        
         <Image
           source={require('./frontend/image/U.png')}
           style={[estilos.vocBase, estilos.posicionU]}
@@ -37,15 +80,16 @@ const Inicio = () =>{
 };
 
 export default Inicio;
+
 const estilos = StyleSheet.create({
-  contPrin:{
+  contPrin: {
     flex: 1,
-    backgroundColor: '#386070',
+    backgroundColor: '#ae56b6',
   },
-    contVoc:{
+  contVoc: {
     flex: 1,
-    backgroundColor: '#a8daee',
-    position:'relative'
+    backgroundColor: '#ae56b6',
+    position: 'relative'
   },
   vocBase: {
     width: scale(55),
@@ -54,39 +98,36 @@ const estilos = StyleSheet.create({
     position: 'absolute'
   },
   posA: {
-    top:'60%',
+    top: '60%',
     left: '15%',
-    transform: [{rotate: '-60deg'}]
   },
   posicionE: {
-    top: '25%', 
+    top: '25%',
     left: '27%',
     transform: [{ rotate: '-35deg' }]
   },
   posicionI: {
-    top: '15%', 
-    left: '45%', 
+    top: '15%',
+    left: '45%',
     transform: [{ rotate: '0deg' }]
   },
   posicionO: {
-    top: '25%', 
-    right: '27%', 
+    top: '25%',
+    right: '27%',
     transform: [{ rotate: '35deg' }]
   },
   posicionU: {
-    top: '62%', 
+    top: '62%',
     right: '17%',
     transform: [{ rotate: '60deg' }]
   },
   contPlay: {
     width: scale(80),
     height: verticalScale(80),
-    resizeMode: 'contain',
     position: 'absolute'
   },
   Play: {
-    top: '60%', 
-    left: '44%', 
-    transform: [{ rotate: '0deg' }]
+    top: '60%',
+    left: '44%',
   },
-})
+});
